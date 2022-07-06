593eb1b19d51b7d1ddf2bf92f8c1d2e5c6628f64
jvelazquez <jonathanvgms@gmail.com>
2022-07-06 08:16:26 -0300

    Initial commit

---
+++ SingleSignOn/Controllers/UsuarioController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SingleSignOn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        List<string> usuarios = new List<string> { "usuario1", "usuario2" };
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(usuarios);
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] string usuario)
+        {
+            var usuarioActual = usuarios.First();
+            usuarioActual = usuario;
+            return Ok(); 
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] string usuario)
+        {
+            this.usuarios.Add(usuario);
+            return StatusCode(201);
+        }
+
+        [HttpDelete]
+        public ActionResult Delete([FromBody] string usuario)
+        {
+            var user = usuarios.Find(x => x == usuario);
+            usuarios.Remove(user);
+            return StatusCode(201);
+        }
+    }
+}
